3987d242ae77e6f31831da0598ea633b763b9e25
mco345 <mco345@naver.com>
2023-01-31 23:31:44 +0900

    last commit

--- app/src/main/java/com/example/simplewebbrowser/MainActivity.kt
+++ app/src/main/java/com/example/simplewebbrowser/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.example.simplewebbrowser
 
 import android.annotation.SuppressLint
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.inputmethod.EditorInfo
+import android.webkit.URLUtil
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.EditText
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.ContentLoadingProgressBar
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,12 +27,20 @@ class MainActivity : AppCompatActivity() {
         findViewById(R.id.goForwardButton)
     }
 
+    private val refreshLayout: SwipeRefreshLayout by lazy{
+        findViewById(R.id.refreshLayout)
+    }
+
     private val addressBar: EditText by lazy{
         findViewById(R.id.addressBar)
     }
 
     private val webView: WebView by lazy{
         findViewById(R.id.webView)
+    }
+
+    private val progressBar: ContentLoadingProgressBar by lazy{
+        findViewById(R.id.progressBar)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -50,9 +62,13 @@ class MainActivity : AppCompatActivity() {
 
     @SuppressLint("SetJavaScriptEnabled")
     private fun initViews(){
-        webView.webViewClient = WebViewClient() // 외부 웹브라우저 앱으로 이동 막기
-        webView.settings.javaScriptEnabled = true   // 자바 스크립트 사용하겠다고 명시
-        webView.loadUrl(DEFAULT_URL)
+        webView.apply {
+            webViewClient = WebViewClient() // 외부 웹브라우저 앱으로 이동 막기
+            webChromeClient = WebChromeClient()
+            settings.javaScriptEnabled = true   // 자바 스크립트 사용하겠다고 명시
+            loadUrl(DEFAULT_URL)
+        }
+
     }
 
     private fun bindViews(){
@@ -62,7 +78,13 @@ class MainActivity : AppCompatActivity() {
 
         addressBar.setOnEditorActionListener{v, actionId, event ->
             if(actionId == EditorInfo.IME_ACTION_DONE){
-                webView.loadUrl(v.text.toString())
+                val loadingUrl = v.text.toString()
+                if(URLUtil.isNetworkUrl(loadingUrl)){
+                    webView.loadUrl(loadingUrl)
+                }else{
+                    webView.loadUrl("http://$loadingUrl")
+                }
+
             }
 
             return@setOnEditorActionListener false
@@ -76,6 +98,38 @@ class MainActivity : AppCompatActivity() {
             webView.goForward()
         }
 
+        refreshLayout.setOnRefreshListener {
+            webView.reload()
+        }
+
+    }
+
+    inner class WebViewClient: android.webkit.WebViewClient(){
+        // 페이지 로딩 시작
+        override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+            super.onPageStarted(view, url, favicon)
+
+            progressBar.show()
+        }
+
+        // 페이지 로딩 끝난 뒤
+        override fun onPageFinished(view: WebView?, url: String?) {
+            super.onPageFinished(view, url)
+
+            refreshLayout.isRefreshing = false
+            progressBar.hide()
+            goBackButton.isEnabled = webView.canGoBack()
+            goForwardButton.isEnabled = webView.canGoForward()
+            addressBar.setText(url)
+        }
+    }
+
+    inner class WebChromeClient: android.webkit.WebChromeClient(){
+        override fun onProgressChanged(view: WebView?, newProgress: Int) {
+            super.onProgressChanged(view, newProgress)
+
+            progressBar.progress = newProgress
+        }
     }
 
     companion object{
